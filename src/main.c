d82223a1e762b5da985c880ccecb4ef8f2134b19
Ben Hudson <bhudson@getpebble.com>
2014-11-06 17:58:21 -0800

    added chunking for large images

--- src/main.c
+++ src/main.c
@@ -8,18 +8,48 @@ static GBitmap *bitmap;
 static char text[8];
 
 typedef enum {
-  APP_KEY_INCOMING,
-  APP_KEY_OUTGOING,
+  APP_KEY_SIZE,
+  APP_KEY_DATA,
 } AppKey;
 
 static void received_image(DictionaryIterator *it, void *ctx) {
-  Tuple *tuple = dict_read_first(it);
-  char *buffer = malloc(tuple->length);
-  memcpy(buffer, tuple->value->data, tuple->length);
+  static char *buffer = NULL;
+  static uint16_t total = 0;
+  static uint16_t index = 0;
 
-  gbitmap_destroy(bitmap);
-  bitmap = gbitmap_create_with_png_data((uint8_t *)buffer, tuple->length);
-  layer_mark_dirty(bitmap_layer_get_layer(bitmap_layer));
+  Tuple *tuple = dict_read_first(it);
+  while (tuple) {
+    switch (tuple->key) {
+      case APP_KEY_SIZE: {
+        if (buffer) {
+          APP_LOG(APP_LOG_LEVEL_DEBUG, "freeing buffer");
+          free(buffer);
+        }
+        total = tuple->value->uint16;
+        buffer = malloc(total);
+        index = 0;
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "allocating buffer of %d bytes", total);
+      }
+      break;
+      case APP_KEY_DATA: {
+        if (buffer) {
+          memcpy(buffer + index, tuple->value->data, tuple->length);
+          index += tuple->length;
+          APP_LOG(APP_LOG_LEVEL_DEBUG, "receiving bytes %d/%d", index, total);
+
+          if (index >= total) {
+            gbitmap_destroy(bitmap);
+            bitmap = gbitmap_create_with_png_data((uint8_t *)buffer, total);
+            layer_mark_dirty(bitmap_layer_get_layer(bitmap_layer));
+            buffer = NULL;
+            index = 0;
+          }
+        }
+      }
+      break;
+    }
+    tuple = dict_read_next(it);
+  }
 }
 
 static void update_time(struct tm* time, TimeUnits units) {
@@ -30,7 +60,7 @@ static void update_time(struct tm* time, TimeUnits units) {
 static void request_image(AccelAxisType axis, int32_t direction) {
   DictionaryIterator *it;
   app_message_outbox_begin(&it);
-  dict_write_int(it, APP_KEY_OUTGOING, 0, sizeof(int), true);
+  dict_write_int(it, APP_KEY_DATA, 0, sizeof(int), true);
   app_message_outbox_send();
 }
 
@@ -61,12 +91,9 @@ static void window_unload(Window *window) {
 
 static void init(void) {
   app_comm_set_sniff_interval(SNIFF_INTERVAL_REDUCED);
-
   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
   app_message_register_inbox_received(received_image);
-
   tick_timer_service_subscribe(MINUTE_UNIT, update_time);
-
   accel_tap_service_subscribe(request_image);
 
   window = window_create();
